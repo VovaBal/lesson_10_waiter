658202db915daf3f900cba54b845205ae16375ff
Vova Balasheskul <balasevolodimir@gmail.com>
2024-04-05 09:42:58 +0300

    add waiters

---
+++ src/test/java/uitest/BaseTest.java
@@ -0,0 +1,26 @@
+package uitest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseTest {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @AfterClass
+    public void afterClass() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
